e38943bf9be5e0970a08e2bf7324c23ad40b4b11
Fernando Emanuel Stefanovic Carroza <stefanovicfernando@gmail.com>
2024-06-17 16:42:25 -0300

    Create scene.py

---
+++ scene.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Scene:
+    def __init__(self, screen):
+        self.screen = screen
+    def handle_event(self, event):
+        pass
+    def update(self):
+        pass
+    def draw(self):
+        pass
